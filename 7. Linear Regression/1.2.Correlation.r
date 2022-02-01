cb91c0ab5f981a7b704a0514bf221cdaa00c297b
YardenBens7 <96015900+YardenBens7@users.noreply.github.com>
2022-02-01 21:23:40 +0200

    Delete 7. Linear Regression directory

--- 7. Linear Regression/1.2.Correlation.r
+++
@@ -1,9 +0,0 @@
-install.packages("lahman")
-library(Lahman)
-library(dplyr)
-
-Teams %>% filter(yearID %in% c(1961:2001)) %>% summarize(r = cor(R/G, AB/G))
-
-Teams %>% filter(yearID %in% c(1961:2001)) %>% summarize(r = cor(W/G, E/G))
-
-Teams %>% filter(yearID %in% c(1961:2001)) %>% summarize(r = cor(X2B/G, X3B/G))
